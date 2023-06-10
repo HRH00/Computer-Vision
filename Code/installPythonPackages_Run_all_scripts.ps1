c2aeb7654951155141e0456019b2b7a1a579a2e4
HRH00 <45105662+HRH00@users.noreply.github.com>
2023-06-10 13:52:46 +0100

    refactor

---
+++ Code/installPythonPackages_Run_all_scripts.ps1
@@ -0,0 +1,49 @@
+# redirect stderr into stdout
+$p = &{python -V} 2>&1
+# check if an ErrorRecord was returned
+if($p -is [System.Management.Automation.ErrorRecord])
+{
+    Write-Host "Install python 3.11" 
+    # grab the version string from the error message
+    $p.Exception.Message
+	Write-Host "Press any key to continue..."
+	$Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+}
+
+else 
+{
+    # otherwise return as is
+    Write-Host "Python Version is:" 
+
+    $p
+    Write-Host "Downloading pip" 
+
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    Write-Host "running get pip" 
+    python get-pip.py
+    Write-Host "installing pip" 
+
+    python -m pip install --upgrade pip
+    Write-Host "installing scikit-learn"
+    pip install scikit-learn
+    Write-Host "installing matplotlib"
+    pip install matplotlib
+    Write-Host "installing opencv-contrib-python"
+    pip install opencv-contrib-python
+    Write-Host "installing numpy"
+    pip install numpy
+
+    #run the python scripts
+    Write-Host "Running HOG_and_SVM_classifier_trainer.py with Python" 
+    python Code\\HOG_and_SVM_classifier_trainer.py
+    Write-Host "Running HOG_and_SVM_classifier_tester.py with Python" 
+    python Code\\HOG_and_SVM_classifier_tester.py
+    Write-Host "Running SIFT_and_SVM_classifier_trainer.py with Python" 
+    python Code\\SIFT_and_SVM_classifier_trainer.py
+    Write-Host "Running SIFT_and_SVM_classifier_tester.py with Python" 
+    python Code\\SIFT_and_SVM_classifier_tester.py
+
+    write-host "Script Complete"
+
+
+}
