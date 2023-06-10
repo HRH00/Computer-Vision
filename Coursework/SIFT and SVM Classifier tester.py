e292298206299172cda461d5d5b56f0c4d406991
HRH00 <45105662+HRH00@users.noreply.github.com>
2023-06-10 01:52:37 +0100

    confusion matrix

--- Coursework/SIFT and SVM Classifier tester.py
+++ Coursework/SIFT and SVM Classifier tester.py
@@ -2,6 +2,9 @@ import os
 import pickle
 from sklearn.metrics import accuracy_score
  
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
+
 def main():
     print("Testing SIFT and SVM Classifier")
     # Test SVM classifier
@@ -21,7 +24,13 @@ def main():
         accuracy = accuracy_score(y_test, predictions)
         print("Accuracy:", accuracy)
         print("Done\n")
-
+        
+        print("confusion matrtix")
+        cm = confusion_matrix(y_test, predictions)
+        print("Confusion Matrix:")
+        print(cm)
+        # Precision, recall, f-score 
+        print(classification_report(y_test, predictions))
     except: # ERROR
         print("Cannot load Classifier - run HOG_and_SVM_classifier_trainer.py first")
     
